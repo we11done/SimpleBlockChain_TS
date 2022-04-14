cf3c27e5dca3d7e75c9fca8b43356864016a0b31
Jaehyun Jun <wjswogus0007@gmail.com>
2022-04-13 23:06:56 -0400

    created Block class and first block

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock: Block = new Block(
+  0,
+  '123213123',
+  '',
+  'my_first_block',
+  123456
+);
+
+let blockChain: [Block] = [genesisBlock];
+
+export {};
